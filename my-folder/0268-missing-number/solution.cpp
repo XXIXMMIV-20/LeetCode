3ab0d9ce5500f46a1e2de70437097119260fe609
Shashank shekhar Patnaik <shashankshekharpatnaik20@gmail.com>
2026-05-20 07:54:41 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 21.9 MB (37.52%)

--- my-folder/0268-missing-number/solution.cpp
+++ my-folder/0268-missing-number/solution.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        vector<int> hash(nums.size() + 1 , 0);
-        for(int i = 0; i < nums.size(); i++) hash[nums[i]] += 1; 
-        for(int i = 0; i <= nums.size(); i++) if(hash[i] == 0) return i;
-        return -1;
+        int sum = 0;
+        int nsum = 0;
+        for(int i = 0; i < nums.size(); i++) sum += nums[i]; 
+        for(int i = 0; i <= nums.size(); i++) nsum += i; 
+        return nsum - sum;
     }
 };
