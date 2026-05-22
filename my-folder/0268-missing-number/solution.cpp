87419cb3ba915c8de46b9199d25ce3091f49f62d
Shashank shekhar Patnaik <shashankshekharpatnaik20@gmail.com>
2026-05-22 08:06:21 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 21.9 MB (13.11%)

--- my-folder/0268-missing-number/solution.cpp
+++ my-folder/0268-missing-number/solution.cpp
@@ -1,10 +1,13 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int sum = 0;
-        int nsum = 0;
-        for(int i = 0; i < nums.size(); i++) sum += nums[i]; 
-        for(int i = 0; i <= nums.size(); i++) nsum += i; 
-        return nsum - sum;
+        int xor1 = 0;
+        int xor2 = 0;
+        int n = nums.size();
+        for(int i = 0; i < n; i++){
+            xor1 ^= nums[i];
+            xor2 ^= (i + 1);
+        }
+        return xor1 ^ xor2;
     }
 };
